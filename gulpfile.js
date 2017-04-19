b56164abe9e52b7b11963576edcb729bd792a021
wangxaiokang <524920914@qq.com>
2017-04-19 15:26:07 +0800

    加入了gulp构建项目

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
-var gulp = require('gulp');
+// 载入外挂
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    cleanDest = require('gulp-clean-dest'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    copy = require('gulp-copy'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
-gulp.task('build', function(){
-    console.log('buld task!');
+// 项目源代码路径
+var path = {};
+var src = path.src = './src/**/*.js';
+
+// 项目构建目标路径
+var dest = path.dest = './dist';
+
+// 编译
+gulp.task('default', function () {
+    return gulp.src(src) // 读取源码
+        .pipe(cleanDest(dest))
+        .pipe(gulp.dest(dest)) // 写入目标
+        .pipe(rename({ suffix: '.min' })) //重命名
+        .pipe(uglify()) // 压缩
+        .pipe(gulp.dest(dest)) // 写入目标
+        .pipe(notify({ message: 'build task complete' })); //提示完成
 });
